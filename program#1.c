e0f20cd08f80934f6aaf214c2c0f0ab0e2729a93
template <teaypvrn@gmail.com>
2018-03-28 01:29:23 +0700

    modified program#1.c

--- program#1.c
+++ program#1.c
@@ -2,5 +2,6 @@
 int main()
 {
 	printf("Hello world");
-	printf("Change something in program 1");
+	printf("Change something in program 1...");
+	printf("I really need to go to The script concert but I have no money");
 }
